99430971f968d896df9d4edcf20da91ee6e1bd50
Josh DeGraw <18509575+josh-degraw@users.noreply.github.com>
2024-06-27 15:20:44 -0400

    Set up Featured Product section (#25)
    * Add hero image, clean up some things
    
    * Use featured collection image directly
    
    * Fix some style issues

--- app/components/Icon.tsx
+++ app/components/Icon.tsx
@@ -24,7 +24,9 @@ export default function Icon({name, className, ...props}: IconProps) {
   return (
     <svg
       className={cn('size-6 text-black dark:text-white', className)}
-      aria-hidden
+      aria-hidden={
+        props['aria-label'] ? undefined : props['aria-hidden'] || true
+      }
       {...props}
     >
       <use href={`/sprite.svg#${name}`} />
--- storefrontapi.generated.d.ts
+++ storefrontapi.generated.d.ts
@@ -281,18 +281,13 @@ export type FeaturedCollectionQueryVariables = StorefrontAPI.Exact<{
 }>;
 
 export type FeaturedCollectionQuery = {
-  collections: {
-    nodes: Array<
-      Pick<StorefrontAPI.Collection, 'id' | 'title' | 'handle'> & {
-        image?: StorefrontAPI.Maybe<
-          Pick<
-            StorefrontAPI.Image,
-            'id' | 'url' | 'altText' | 'width' | 'height'
-          >
-        >;
-      }
-    >;
-  };
+  collectionByHandle?: StorefrontAPI.Maybe<
+    Pick<StorefrontAPI.Collection, 'id' | 'title' | 'handle'> & {
+      image?: StorefrontAPI.Maybe<
+        Pick<StorefrontAPI.Image, 'id' | 'url' | 'altText' | 'width' | 'height'>
+      >;
+    }
+  >;
 };
 
 export type RecommendedProductFragment = Pick<
@@ -1063,7 +1058,7 @@ interface GeneratedQueryTypes {
     return: SitemapQuery;
     variables: SitemapQueryVariables;
   };
-  '#graphql\n  fragment FeaturedCollection on Collection {\n    id\n    title\n    image {\n      id\n      url\n      altText\n      width\n      height\n    }\n    handle\n  }\n  query FeaturedCollection($country: CountryCode, $language: LanguageCode)\n    @inContext(country: $country, language: $language) {\n    collections(first: 1, sortKey: UPDATED_AT, reverse: true) {\n      nodes {\n        ...FeaturedCollection\n      }\n    }\n  }\n': {
+  '#graphql\n  fragment FeaturedCollection on Collection {\n      id\n      title\n      image {\n        id\n        url\n        altText\n        width\n        height\n      }\n      handle\n    }\n\n  query FeaturedCollection($country: CountryCode, $language: LanguageCode)\n    @inContext(country: $country, language: $language) {\n    collectionByHandle(handle: "featured") {\n      ...FeaturedCollection\n    }\n  }\n': {
     return: FeaturedCollectionQuery;
     variables: FeaturedCollectionQueryVariables;
   };
