fc27420ec1edb8c8b62eeb3194ad005821a836c0
Josh DeGraw <18509575+josh-degraw@users.noreply.github.com>
2024-06-27 09:12:37 -0400

    Add theme back in

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,14 +2,48 @@ import type {Config} from 'tailwindcss';
 
 const config = {
   darkMode: ['class'],
-  content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
-  ],
+  content: ['./app/**/*.{js,jsx,ts,tsx}'],
   prefix: '',
   theme: {
+    fontFamily: {
+      sans: [
+        'Inter',
+        'ui-sans-serif',
+        'system-ui',
+        'sans-serif',
+        '"Apple Color Emoji"',
+        '"Segoe UI Emoji"',
+        '"Segoe UI Symbol"',
+        '"Noto Color Emoji"',
+      ],
+    },
+    colors: {
+      cyan: {brand: '#3DEFE9'},
+      blue: {brand: '#3992FF'},
+      green: {brand: '#6BD968'},
+      yellow: {brand: '#FECC1B'},
+      magenta: {brand: '#D83BD2'},
+      red: {brand: '#F44250'},
+      'shop-pay': {brand: '#5A31F4'},
+      success: {brand: '#3992FF'},
+      error: {brand: '#F44250'},
+      warning: {brand: '#FECC1B'},
+
+      white: '#FFFFFF',
+      black: '#000000',
+
+      neutral: {
+        50: '#F5F5EF',
+        100: '#EAEAE4',
+        200: '#E3E2DD',
+        300: '#DADADA',
+        400: '#808080',
+        500: '#59585C',
+        600: '#434343',
+        700: '#343436',
+        800: '#242426',
+      },
+    },
     container: {
       center: true,
       padding: '2rem',
