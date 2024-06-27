b00d9dd37a26772c6789cec2bb2ca892de812be2
Juan P. Prieto <jp@calltheguys.co>
2024-06-27 14:13:29 -0400

    Rename plugin

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,8 @@ import {hydrogen} from '@shopify/hydrogen/vite';
 import {oxygen} from '@shopify/mini-oxygen/vite';
 import {vitePlugin as remix} from '@remix-run/dev';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import {createSvgSpritePlugin} from '@mcansh/vite-svg-sprite-plugin';
+import {removeHashFromSprite} from './plugins/vite-sprite-svg-fix';
 
 export default defineConfig({
   plugins: [
@@ -17,6 +19,8 @@ export default defineConfig({
       },
     }),
     tsconfigPaths(),
+    createSvgSpritePlugin(),
+    removeHashFromSprite(),
   ],
   build: {
     // Allow a strict Content-Security-Policy
