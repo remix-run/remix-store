e2d45f41f85163d5a0c98f67567219f9c1c7eba1
Brooks Lybrand <brookslybrand@gmail.com>
2024-06-28 10:33:37 -0400

    Add privacy policy page

--- app/routes/($locale).policies._index.tsx
+++ app/routes/($locale).policies._index.tsx
@@ -1,62 +1,60 @@
 import {json, type LoaderFunctionArgs} from '@shopify/remix-oxygen';
 import {useLoaderData, Link} from '@remix-run/react';
+import {Image} from '~/components/Image';
 
 export async function loader({context}: LoaderFunctionArgs) {
-  const data = await context.storefront.query(POLICIES_QUERY);
-  const policies = Object.values(data.shop || {});
+  const data = await context.storefront.query(POLICIES_QUERY, {
+    cache: context.storefront.CacheNone(),
+  });
 
-  if (!policies.length) {
+  const {privacyPolicy} = data.shop;
+
+  if (!privacyPolicy) {
     throw new Response('No policies found', {status: 404});
   }
 
-  return json({policies});
+  return json({privacyPolicy});
 }
 
 export default function Policies() {
-  const {policies} = useLoaderData<typeof loader>();
+  const {privacyPolicy} = useLoaderData<typeof loader>();
 
   return (
-    <div className="policies">
-      <h1>Policies</h1>
-      <div>
-        {policies.map((policy) => {
-          if (!policy) return null;
-          return (
-            <fieldset key={policy.id}>
-              <Link to={`/policies/${policy.handle}`}>{policy.title}</Link>
-            </fieldset>
-          );
-        })}
+    <div className="px-9 pb-12">
+      <Image
+        className="rounded-3xl"
+        // TODO: figure out how to get this data from GraphQL
+        data={{
+          url: 'https://cdn.shopify.com/s/files/1/0655/4127/5819/files/privacy-banner.png?v=1719583076',
+          width: 1368,
+          height: 300,
+          altText: '',
+        }}
+      />
+      <div className="p-12 rounded-3xl bg-neutral-100 dark:bg-gray mt-3">
+        <h1>{privacyPolicy.title}</h1>
+        <div
+          className="flex flex-col gap-9 pt-9 policy-container"
+          dangerouslySetInnerHTML={{__html: privacyPolicy.body}}
+        />
       </div>
     </div>
   );
 }
 
 const POLICIES_QUERY = `#graphql
-  fragment PolicyItem on ShopPolicy {
+  fragment Policy on ShopPolicy {
+    body
+    handle
     id
     title
-    handle
+    url
   }
-  query Policies ($country: CountryCode, $language: LanguageCode)
+  query PrivacyPolicy ($country: CountryCode, $language: LanguageCode)
     @inContext(country: $country, language: $language) {
     shop {
       privacyPolicy {
-        ...PolicyItem
-      }
-      shippingPolicy {
-        ...PolicyItem
-      }
-      termsOfService {
-        ...PolicyItem
-      }
-      refundPolicy {
-        ...PolicyItem
-      }
-      subscriptionPolicy {
-        id
-        title
-        handle
+        ...Policy
       }
     }
   }
