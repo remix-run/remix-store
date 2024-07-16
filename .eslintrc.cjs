49e951cbfdb6d98fb9389ef702b387d53219f25d
Brooks Lybrand <brookslybrand@gmail.com>
2024-07-16 12:17:52 -0500

    Remove Shopify prettier and add tailwind classname sorting

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,17 +3,18 @@
  */
 module.exports = {
   extends: [
-    '@remix-run/eslint-config',
-    'plugin:hydrogen/recommended',
-    'plugin:hydrogen/typescript',
+    "@remix-run/eslint-config",
+    "plugin:hydrogen/recommended",
+    "plugin:hydrogen/typescript",
   ],
   rules: {
-    '@typescript-eslint/ban-ts-comment': 'off',
-    '@typescript-eslint/naming-convention': 'off',
-    'hydrogen/prefer-image-component': 'off',
-    'no-useless-escape': 'off',
-    '@typescript-eslint/no-non-null-asserted-optional-chain': 'off',
-    'no-case-declarations': 'off',
-    'no-console': ['warn', {allow: ['warn', 'error']}],
+    "@typescript-eslint/ban-ts-comment": "off",
+    "@typescript-eslint/naming-convention": "off",
+    "hydrogen/prefer-image-component": "off",
+    "no-useless-escape": "off",
+    "@typescript-eslint/no-non-null-asserted-optional-chain": "off",
+    "no-case-declarations": "off",
+    "no-console": ["warn", { allow: ["warn", "error"] }],
+    "prettier/prettier": "off",
   },
 };
