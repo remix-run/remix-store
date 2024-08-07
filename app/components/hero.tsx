618554804ccbd969a6e5563654461139dc656a21
Brooks Lybrand <brookslybrand@gmail.com>
2024-08-07 11:28:29 -0500

    Fix overlays for hero

--- app/components/hero.tsx
+++ app/components/hero.tsx
@@ -24,18 +24,25 @@ export function Hero() {
         }}
       >
         <div
-          className="h-[192px] w-full rounded-3xl sm:h-[300px]"
-          // TODO: get from Shopify CDN
+          className="relative h-[192px] w-full overflow-hidden rounded-3xl bg-[#C8DFEA] sm:h-[300px] sm:bg-none"
           style={{
-            background: `url(${ledMiniSkateboard}) no-repeat center center`,
-            backgroundSize: "cover",
+            boxShadow:
+              "0px 2px 0px 0px #6A98A7, 0px 4px 6px 0px rgba(85, 164, 175, 0.50) inset, 0px -2px 1px 0px rgba(172, 255, 252, 0.30) inset, 0px 3px 2px 0px rgba(105, 255, 249, 0.20) inset, 0px 0px 30px 0px rgba(105, 255, 249, 0.50) inset, 0px 1px 1px 0px rgba(0, 0, 0, 0.40), 0px 5px 2px 0px rgba(255, 255, 255, 0.10)",
           }}
         >
-          <div className="flex h-full max-w-[484px] flex-col justify-center gap-2 p-6 text-center sm:pl-9 sm:text-left lg:pl-[100px]">
-            <p className="font-sometype-mono hero-subtitle text-xs md:text-base">
+          <div
+            className="absolute h-full w-full opacity-50 sm:opacity-100"
+            // TODO: get from Shopify CDN
+            style={{
+              background: `linear-gradient(90deg, rgba(0, 0, 0, 0.50) 0%, rgba(0, 0, 2, 0.50) 4.03%, rgba(47, 47, 46, 0.50) 22.5%, rgba(91, 93, 99, 0.50) 40.35%, rgba(105, 107, 110, 0.50) 55.69%, rgba(89, 90, 94, 0.50) 63.06%, rgba(45, 45, 49, 0.50) 79.31%, rgba(0, 0, 3, 0.50) 96.74%, rgba(17, 17, 19, 0.50) 100%), url(${ledMiniSkateboard}) lightgray 50% / cover no-repeat`,
+              backgroundBlendMode: "screen, normal",
+            }}
+          />
+          <div className="absolute flex h-full max-w-[484px] flex-col justify-center gap-2 p-6 text-center sm:pl-9 sm:text-left lg:pl-[100px]">
+            <p className="hero-subtitle font-sometype-mono text-xs md:text-base">
               Build your very own
             </p>
-            <h1 className="font-jersey-10 hero-title text-[64px] md:text-[84px]">
+            <h1 className="hero-title font-jersey-10 text-[64px] md:text-[84px]">
               Remix Mini Skateboard
             </h1>
           </div>
