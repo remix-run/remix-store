870f17fb99399b4f9906b15a915b717100802134
Brooks Lybrand <brookslybrand@gmail.com>
2024-08-13 08:46:31 -0500

    Upgrade dependencies

--- app/components/ui/dropdown-menu.tsx
+++ app/components/ui/dropdown-menu.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import * as DropdownMenuPrimitive from "@radix-ui/react-dropdown-menu";
+// TODO: replace these imports with our own icons
 import { Check, ChevronRight, Circle } from "lucide-react";
 
 import { cn } from "~/lib";
