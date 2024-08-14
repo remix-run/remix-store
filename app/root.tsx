24c0c84775104479b4ae1ee7b06197af003f6837
Brooks Lybrand <brookslybrand@gmail.com>
2024-08-14 14:19:25 -0500

    Add new favicon

--- app/root.tsx
+++ app/root.tsx
@@ -75,11 +75,83 @@ export function links() {
     })),
     ...googleFonts.map((href) => ({ rel: "preload", as: "style", href })),
     ...googleFonts.map((href) => ({ rel: "stylesheet", href })),
-    { rel: "icon", href: "/favicon-32.png", sizes: "32x32" },
-    { rel: "icon", href: "/favicon-128.png", sizes: "128x128" },
-    { rel: "icon", href: "/favicon-180.png", sizes: "180x180" },
-    { rel: "icon", href: "/favicon-192.png", sizes: "192x192" },
-    { rel: "apple-touch-icon", href: "/favicon-180.png", sizes: "180x180" },
+    {
+      rel: "apple-touch-icon-precomposed",
+      href: "/apple-touch-icon-57x57.png",
+      sizes: "57x57",
+    },
+    {
+      rel: "apple-touch-icon-precomposed",
+      href: "/apple-touch-icon-114x114.png",
+      sizes: "114x114",
+    },
+    {
+      rel: "apple-touch-icon-precomposed",
+      href: "/apple-touch-icon-72x72.png",
+      sizes: "72x72",
+    },
+    {
+      rel: "apple-touch-icon-precomposed",
+      href: "/apple-touch-icon-144x144.png",
+      sizes: "144x144",
+    },
+    {
+      rel: "apple-touch-icon-precomposed",
+      href: "/apple-touch-icon-60x60.png",
+      sizes: "60x60",
+    },
+    {
+      rel: "apple-touch-icon-precomposed",
+      href: "/apple-touch-icon-120x120.png",
+      sizes: "120x120",
+    },
+    {
+      rel: "apple-touch-icon-precomposed",
+      href: "/apple-touch-icon-76x76.png",
+      sizes: "76x76",
+    },
+    {
+      rel: "apple-touch-icon-precomposed",
+      href: "/apple-touch-icon-152x152.png",
+      sizes: "152x152",
+    },
+    {
+      rel: "icon",
+      type: "image/png",
+      href: "/favicon-196x196.png",
+      sizes: "196x196",
+    },
+    {
+      rel: "icon",
+      type: "image/png",
+      href: "/favicon-96x96.png",
+      sizes: "96x96",
+    },
+    {
+      rel: "icon",
+      type: "image/png",
+      href: "/favicon-32x32.png",
+      sizes: "32x32",
+    },
+    {
+      rel: "icon",
+      type: "image/png",
+      href: "/favicon-16x16.png",
+      sizes: "16x16",
+    },
+    {
+      rel: "icon",
+      type: "image/png",
+      href: "/favicon-128.png",
+      sizes: "128x128",
+    },
+    { name: "application-name", content: "&nbsp;" },
+    { name: "msapplication-TileColor", content: "#FFFFFF" },
+    { name: "msapplication-TileImage", content: "/mstile-144x144.png" },
+    { name: "msapplication-square70x70logo", content: "/mstile-70x70.png" },
+    { name: "msapplication-square150x150logo", content: "/mstile-150x150.png" },
+    { name: "msapplication-wide310x150logo", content: "/mstile-310x150.png" },
+    { name: "msapplication-square310x310logo", content: "/mstile-310x310.png" },
   ];
 }
 
