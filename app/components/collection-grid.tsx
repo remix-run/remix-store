25135f549c0c29f9735f7f86fe2c1409222cf7ce
Brooks Lybrand <brookslybrand@gmail.com>
2024-09-25 11:35:41 -0500

    Product Tiles and Grid v2 (#97)
    * wip products tiles and grid
    
    * Remove aspect ratio
    
    * Fix aspect ratio of images on collection grid
    
    * Fix tag styles
    
    * Fix mobile product image CLS
    
    * Cleanup and make no-gradient no longer an option
    
    * Add opacity to images when sold out
    
    * Adjust grid gaps
    
    * Change gap again
    
    * Fix line-height for product price

--- app/components/collection-grid.tsx
+++ app/components/collection-grid.tsx
@@ -3,60 +3,21 @@ import { Money } from "@shopify/hydrogen";
 import type { RecommendedProductsQuery } from "storefrontapi.generated";
 import { Image } from "~/components/image";
 import { parseGradientColors } from "~/lib/metafields";
-import clsx from "clsx";
 import { cva } from "class-variance-authority";
+import clsx from "clsx";
 
 interface CollectionGridProps {
   products?: RecommendedProductsQuery["products"]["nodes"];
 }
 
-interface TagProps {
-  status: "soldOut" | "onSale" | "new" | "none";
-}
-
-const statusText = {
-  soldOut: "SOLD OUT",
-  onSale: "SALE",
-  new: "NEW",
-};
-
-const tagStyles = cva("absolute left-6 top-5 text-sm font-bold", {
-  variants: {
-    status: {
-      soldOut: "text-neutral-600 dark:text-neutral-300 opacity-50",
-      onSale: "text-red-brand",
-      new: "text-neutral-600 dark:text-neutral-300",
-    },
-  },
-});
-
 export function CollectionGrid({ products }: CollectionGridProps) {
   if (!products) return null;
 
   return (
-    <div
-      className={clsx(
-        // Undo padding on the body for full-width grid
-        "-mx-3 py-6 sm:-mx-9 sm:py-8",
-        "flex justify-center bg-black bg-opacity-5 dark:bg-opacity-20",
-      )}
-    >
-      <div
-        // TODO: validate with design these breakpoints/the in-between states. For example, the desktop is at 1440px
-        className={clsx(
-          "grid",
-          "grid-cols-[repeat(2,_174px)] gap-3",
-          "sm:grid-cols-[repeat(2,_232px)] sm:gap-x-3 sm:gap-y-4",
-          "md:grid-cols-[repeat(3,_232px)]",
-          "lg:grid-cols-[repeat(3,_320px)] lg:gap-3",
-          "xl:grid-cols-[repeat(3,_400px)]",
-          "2xl:grid-cols-[repeat(4,_400px)]",
-        )}
-      >
-        {products.map((product) => (
-          <CollectionItem product={product} key={product.id} />
-        ))}
-      </div>
+    <div className="sm grid grid-cols-2 gap-x-3 gap-y-6 md:grid-cols-3 md:gap-x-[18px] md:gap-y-9 lg:grid-cols-4">
+      {products.map((product) => (
+        <CollectionItem product={product} key={product.id} />
+      ))}
     </div>
   );
 }
@@ -87,44 +48,62 @@ export function CollectionItem({ product }: CollectionItemProps) {
 
   return (
     <Link
-      className="hover:text-inherit hover:no-underline"
+      className="flex flex-col gap-1 no-underline"
       prefetch="intent"
       to={`/products/${handle}`}
     >
-      <div
-        className={clsx(
-          "relative isolate aspect-square w-full overflow-hidden rounded-2xl bg-neutral-100 shadow-yamaha-secondary dark:bg-neutral-700",
-        )}
-      >
+      <div className="relative h-[160px] overflow-hidden rounded-[20px] bg-black bg-opacity-5 md:h-[240px] lg:h-[300px] xl:h-[400px] 2xl:h-[480px] dark:bg-opacity-20">
         <Image
-          aspectRatio="1/1"
           data={product.images.nodes[0]}
-          gradient={gradients[0]}
+          gradient={gradients[0] ?? "random"}
           gradientHover={true}
           sizes="(min-width: 45em) 20vw, 50vw"
+          className={clsx(
+            "max-h-full max-w-full overflow-hidden",
+            status === "soldOut" && "opacity-30",
+          )}
         />
-
         <Tag status={status} />
-
-        <div className="absolute bottom-5 pl-6">
-          <h3 className="mb-2 text-xs font-bold tracking-[-0.96px] md:text-2xl">
-            {title}
-          </h3>
-          <small className="flex gap-2 text-xs">
-            <Money data={price} withoutTrailingZeros />
-            {isOnSale && (
-              <Money
-                data={compareAtPrice}
-                withoutTrailingZeros
-                className="line-through opacity-35"
-              />
-            )}
-          </small>
-        </div>
       </div>
+      <h3 className="text-sm font-semibold leading-[1.2rem] tracking-[-0.28px] text-black sm:text-base sm:leading-none sm:tracking-[-0.32px] md:text-xl md:tracking-[-0.4px] lg:text-2xl lg:tracking-[-0.48px] dark:text-white">
+        {title}
+      </h3>
+      <small className="text-dark flex gap-2 font-mono text-xs font-medium leading-none sm:text-base sm:leading-none dark:text-white">
+        <Money data={price} withoutTrailingZeros />
+        {isOnSale && (
+          <Money
+            data={compareAtPrice}
+            withoutTrailingZeros
+            className="text-neutral-600 text-opacity-35 line-through dark:text-neutral-300 dark:text-opacity-35"
+          />
+        )}
+      </small>
     </Link>
   );
 }
+
+interface TagProps {
+  status: "soldOut" | "onSale" | "new" | "none";
+}
+
+const statusText = {
+  soldOut: "sold out",
+  onSale: "sale",
+  new: "new",
+};
+
+const tagStyles = cva(
+  "absolute text-[0.625rem] left-2 top-2 md:left-4 md:top-4 p-2 md:p-3 md:text-base font-bold rounded-[10px] bg-neutral-200 dark:bg-neutral-800 uppercase",
+  {
+    variants: {
+      status: {
+        soldOut: "text-neutral-600 dark:text-neutral-300",
+        onSale: "text-red-brand",
+        new: "text-blue-brand ",
+      },
+    },
+  },
+);
 
 const Tag = ({ status }: TagProps) => {
   if (status === "none") return null;
