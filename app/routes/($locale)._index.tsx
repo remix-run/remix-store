7df3ba56c6ed81a6fbdf567243aedf6501b15e90
Brooks Lybrand <brookslybrand@gmail.com>
2025-03-12 08:01:26 -0500

    Improve legibility of text in hero component

--- app/routes/($locale)._index.tsx
+++ app/routes/($locale)._index.tsx
@@ -102,7 +102,7 @@ function Hero({ masthead, assetImages, product }: HeroDataProps) {
   );
   let opacity = Math.max(0, 1 - scrollPercentage * 4);
 
-  let highlightSwitch = scrollPercentage > 0.6;
+  let highlightSwitch = scrollPercentage > 0.3;
 
   // Calculate which frame to show based on scroll percentage
   let frameIndex = Math.min(
@@ -113,7 +113,7 @@ function Hero({ masthead, assetImages, product }: HeroDataProps) {
   return (
     <div
       ref={heroRef}
-      className="relative h-screen max-h-[1200px] overflow-hidden bg-linear-[180deg,var(--color-black),#27273B] pt-24 md:top-7 md:h-[1200px] lg:h-[1600px] lg:max-h-[1600px]"
+      className="relative h-screen max-h-[1200px] overflow-hidden bg-linear-[180deg,var(--color-black),#27273B] pt-24 md:top-7 md:h-[1200px] lg:h-[1600px] lg:max-h-[1600px] 2xl:h-[1800px] 2xl:max-h-[1800px]"
     >
       <div
         style={{
@@ -131,7 +131,7 @@ function Hero({ masthead, assetImages, product }: HeroDataProps) {
         />
 
         {/* TODO: add better mobile support */}
-        <h1 className="mt-48 flex max-h-min w-full flex-nowrap items-start justify-center gap-8 px-4 text-2xl font-extrabold text-white md:mt-[300px] md:gap-20 md:px-9 md:text-5xl lg:gap-36 lg:text-8xl xl:text-8xl">
+        <h1 className="mt-48 flex max-h-min w-full flex-nowrap items-start justify-center gap-8 px-4 text-2xl font-extrabold text-white md:mt-[300px] md:gap-20 md:px-9 md:text-5xl lg:gap-36 lg:text-8xl xl:mt-72 xl:gap-72 xl:text-8xl 2xl:mt-80">
           <span className="sr-only">Remix</span>
           <HeroText highlight={!highlightSwitch}>
             software
@@ -208,7 +208,7 @@ let RotatingProduct = memo(
     return (
       <Link
         to={`/products/${product.handle}`}
-        className="3xl:scale-130 3xl:hover:scale-135 absolute top-0 w-full translate-y-0 scale-125 transition-transform duration-200 select-none hover:scale-130 md:translate-y-8 lg:scale-100 lg:hover:scale-105 xl:scale-100 xl:hover:scale-105 2xl:translate-y-24 2xl:scale-110 2xl:hover:scale-115"
+        className="3xl:scale-130 3xl:hover:scale-135 3xl:translate-y-56 absolute top-0 w-full translate-y-14 scale-125 transition-transform duration-200 select-none hover:scale-130 md:translate-y-12 lg:translate-y-20 lg:scale-100 lg:hover:scale-105 xl:scale-100 xl:hover:scale-105 2xl:translate-y-32 2xl:scale-110 2xl:hover:scale-115"
       >
         <span className="sr-only">{product.title}</span>
         {assetImages.map((asset, index) => (
@@ -355,9 +355,9 @@ let loadRunnerImage = {
 function LoadRunner() {
   return (
     <div className="flex h-[390px] items-center justify-center gap-4 bg-[#1E1EC4] md:h-[480px] lg:h-[800px]">
-      <div className="w-[65%]">
+      <div className="3xl:w-[50%] w-[65%] xl:w-[60%] 2xl:w-[55%]">
         <HydrogenImage
-          className="h-full w-full"
+          className="relative left-1/2 h-full w-full max-w-[90%] -translate-x-1/2"
           sizes="65vw"
           data={loadRunnerImage}
         />
