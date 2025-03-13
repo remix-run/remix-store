0b1cd79a2f78ca35eb5b4c77742b79d32c26e520
Brooks Lybrand <brookslybrand@gmail.com>
2025-03-13 11:15:39 -0500

    Simplify image switch animation and cleanup

--- app/components/product-grid.tsx
+++ app/components/product-grid.tsx
@@ -1,9 +1,17 @@
 import { Link } from "@remix-run/react";
 import { Image as HydrogenImage, Money } from "@shopify/hydrogen";
-import type { ProductItemFragment } from "storefrontapi.generated";
-import { useState, useEffect } from "react";
 import { clsx } from "clsx";
 
+import type {
+  ProductImageFragment,
+  ProductItemFragment,
+} from "storefrontapi.generated";
+
+// TODO:
+// - defer product loading
+// - add product grid example to the docs
+// - add product grid for collection pages
+// - remove old, unneeded code
 interface ProductGridProps {
   products: ProductItemFragment[];
 }
@@ -25,29 +33,13 @@ interface ProductGridItemProps {
 }
 
 function ProductGridItem({ product }: ProductGridItemProps) {
-  const { handle, title, variants, availableForSale, tags } = product;
+  const { handle, title, variants } = product;
 
-  const firstVariant = variants.nodes[0];
-  const price = firstVariant.price;
-  const compareAtPrice = firstVariant.compareAtPrice;
-
-  const isOnSale = !!(compareAtPrice && price.amount < compareAtPrice.amount);
-
-  let status = "none";
-  if (!availableForSale) {
-    status = "soldOut";
-  } else if (isOnSale) {
-    status = "onSale";
-  } else if (tags.some((tag) => tag.toLowerCase() === "new")) {
-    status = "new";
-  }
+  const { price } = variants.nodes[0];
 
   return (
     <div className="group relative flex flex-col items-center">
-      <ProductImages
-        images={product.images.nodes}
-        sizes="(min-width: 1536px) 20vw, (min-width: 1280px) 25vw, (min-width: 1024px) 33vw, (min-width: 768px) 50vw, 100vw"
-      />
+      <ProductImages images={product.images.nodes} />
       <div className="flex flex-col items-center justify-center gap-2.5 py-2 text-base leading-none text-white">
         <Link
           prefetch="intent"
@@ -65,36 +57,18 @@ function ProductGridItem({ product }: ProductGridItemProps) {
   );
 }
 
-type LoadingState = "idle" | "pending" | "loaded" | "error";
-
 interface ProductImagesProps {
-  images: ProductItemFragment["images"]["nodes"];
-  sizes: string;
+  images: ProductImageFragment[];
 }
 
-function ProductImages({ images, sizes }: ProductImagesProps) {
-  const [isHovered, setIsHovered] = useState(false);
-  const [hoverImageLoaded, setHoverImageLoaded] =
-    useState<LoadingState>("idle");
+function ProductImages({ images }: ProductImagesProps) {
+  let firstImage = images[0];
+  let secondImage = images.length > 1 ? images[1] : null;
 
-  const firstImage = images[0];
-  const secondImage = images.length > 1 ? images[1] : null;
+  let sizes =
+    "(min-width: 1536px) 20vw, (min-width: 1280px) 25vw, (min-width: 1024px) 33vw, (min-width: 768px) 50vw, 100vw";
 
-  // Preload second image if it exists
-  useEffect(() => {
-    if (hoverImageLoaded !== "idle" || !secondImage) return;
-
-    setHoverImageLoaded("pending");
-
-    const img = new Image();
-    img.addEventListener("load", () => setHoverImageLoaded("loaded"), {
-      once: true,
-    });
-    img.addEventListener("error", () => setHoverImageLoaded("error"), {
-      once: true,
-    });
-    img.src = secondImage.url;
-  }, [secondImage, hoverImageLoaded]);
+  let baseCss = "max-h-[90%] object-cover object-center";
 
   return (
     <div className="relative flex w-[70%] items-center justify-center transition-transform group-hover:scale-105">
@@ -102,19 +76,21 @@ function ProductImages({ images, sizes }: ProductImagesProps) {
         data={firstImage}
         sizes={sizes}
         className={clsx(
-          "relative max-h-[90%] object-cover object-center opacity-100",
-          hoverImageLoaded === "loaded" &&
-            "group-hover:absolute group-hover:opacity-0",
+          baseCss,
+          secondImage &&
+            "relative opacity-100 group-hover:absolute group-hover:opacity-0",
         )}
-        loading={hoverImageLoaded !== "loaded" ? "lazy" : "eager"}
+        loading="lazy"
       />
-      {secondImage && hoverImageLoaded === "loaded" && (
+      {secondImage && (
         <HydrogenImage
           data={secondImage}
           sizes={sizes}
           className={clsx(
-            "absolute max-h-[90%] object-cover object-center opacity-0 group-hover:relative group-hover:opacity-100",
+            baseCss,
+            "absolute opacity-0 group-hover:relative group-hover:opacity-100",
           )}
+          loading="lazy"
         />
       )}
     </div>
