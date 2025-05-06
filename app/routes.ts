0a7917ab20add187aa5f92178994d8a436f0ed83
Brooks Lybrand <brookslybrand@gmail.com>
2025-05-06 12:26:22 -0500

    Update deps (#148)
    * Update remix
    
    * Bump linting and testing related packages
    
    * Bump remaining packages that I'll bump right now

--- app/routes.ts
+++ app/routes.ts
@@ -2,4 +2,6 @@ import { flatRoutes } from "@remix-run/fs-routes";
 import { hydrogenRoutes } from "@shopify/hydrogen";
 import { type RouteConfig } from "@remix-run/route-config";
 
-export default hydrogenRoutes(await flatRoutes()) satisfies RouteConfig;
+const routes = hydrogenRoutes(await flatRoutes()) satisfies RouteConfig;
+
+export default routes;
