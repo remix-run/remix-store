8a2675ab557d3367ce791efe1e86a24dea013543
Brooks Lybrand <brookslybrand@gmail.com>
2025-05-15 08:59:52 -0500

    remove old deps, upgrade existing ones (besides react)

--- vite.config.ts
+++ vite.config.ts
@@ -17,7 +17,6 @@ export default defineConfig({
     iconsSpritesheet({
       inputDir: "app/assets/icons",
       outputDir: "public",
-
       typesOutputFile: "app/components/icon/types.generated.ts",
       withTypes: true,
       iconNameTransformer: (name) =>
@@ -30,20 +29,5 @@ export default defineConfig({
     // Allow a strict Content-Security-Policy
     // without inlining assets as base64:
     assetsInlineLimit: 0,
-  },
-  ssr: {
-    optimizeDeps: {
-      /**
-       * Include dependencies here if they throw CJS<>ESM errors.
-       * For example, for the following error:
-       *
-       * > ReferenceError: module is not defined
-       * >   at /Users/.../node_modules/example-dep/index.js:1:1
-       *
-       * Include 'example-dep' in the array below.
-       * @see https://vitejs.dev/config/dep-optimization-options
-       */
-      include: ["set-cookie-parser", "cookie", "spin-delay"],
-    },
   },
 });
