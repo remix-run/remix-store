0b7e550c15a772c3259027642ded95e9d32f8e1b
Brooks Lybrand <brookslybrand@gmail.com>
2025-05-15 17:15:33 -0500

    Change "Shop" to "Shop All" on empty cart page

--- app/routes/pages/($locale).cart.tsx
+++ app/routes/pages/($locale).cart.tsx
@@ -129,7 +129,7 @@ export default function Cart() {
               fill="currentColor"
               aria-hidden="true"
             />
-            <span>Shop</span>
+            <span>Shop All</span>
           </AnimatedLinkSpread>
         </div>
       </div>
