956f38c933db364d3fc44db64ae6bafbad9f0890
Brooks Lybrand <brookslybrand@gmail.com>
2025-05-19 12:19:43 -0500

    Make background black

--- app/routes/pages/($locale).products.$handle.tsx
+++ app/routes/pages/($locale).products.$handle.tsx
@@ -52,12 +52,13 @@ export function meta({
 
   let image = product.images.nodes[0].url;
 
-  // if (image) {
-  //   let url = new URL(image);
-  //   url.searchParams.set("width", "1200");
-  //   url.searchParams.set("height", "630");
-  //   image = url.toString();
-  // }
+  if (image) {
+    let url = new URL(image);
+    url.searchParams.set("width", "1200");
+    url.searchParams.set("height", "630");
+    url.searchParams.set("pad_color", "000000");
+    image = url.toString();
+  }
 
   return generateMeta({
     // I think there's a better way to get seo data using some Hydrogen helpers
