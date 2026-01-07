7290fd8e8f7e310327cdd3d4e44b627a2d4ba049
Brooks Lybrand <brookslybrand@gmail.com>
2026-01-07 14:47:22 -0600

    Override hydrogen config

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,8 +1,30 @@
 import type { Config } from "@react-router/dev/config";
 import { hydrogenPreset } from "@shopify/hydrogen/react-router-preset";
 
+// Renames stabilized future flags from Hydrogen's outdated unstable_ names
+function patchedHydrogenPreset() {
+  const preset = hydrogenPreset();
+  return {
+    ...preset,
+    async reactRouterConfig(
+      args: Parameters<NonNullable<typeof preset.reactRouterConfig>>[0],
+    ) {
+      const config = await preset.reactRouterConfig!(args);
+
+      return {
+        ...config,
+        future: {
+          v8_splitRouteModules: true,
+          v8_viteEnvironmentApi: true,
+          v8_middleware: true,
+        } satisfies Config["future"],
+      };
+    },
+  };
+}
+
 export default {
-  presets: [hydrogenPreset()],
+  presets: [patchedHydrogenPreset()],
   future: {
     unstable_optimizeDeps: true,
   },
