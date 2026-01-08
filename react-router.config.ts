b690d556f575f2b4cd50cfacffc0ec0e3487b4d3
Brooks Lybrand <brookslybrand@gmail.com>
2026-01-08 15:57:59 -0600

    Revert "Remove patched preset"
    This reverts commit 998db136ba93bc994a0b333d5dcf20b9373405e2.

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,8 +1,32 @@
 import type { Config } from "@react-router/dev/config";
 import { hydrogenPreset } from "@shopify/hydrogen/react-router-preset";
 
+// Renames stabilized future flags from Hydrogen's outdated unstable_ names
+function patchedHydrogenPreset() {
+  const preset = hydrogenPreset();
+  return {
+    ...preset,
+    async reactRouterConfig(
+      args: Parameters<NonNullable<typeof preset.reactRouterConfig>>[0],
+    ) {
+      const config = await preset.reactRouterConfig!(args);
+
+      return {
+        ...config,
+        future: {
+          v8_middleware: true,
+          v8_splitRouteModules: true,
+          v8_viteEnvironmentApi: false,
+          unstable_optimizeDeps: true,
+          unstable_subResourceIntegrity: false,
+        } satisfies Config["future"],
+      };
+    },
+  };
+}
+
 export default {
-  presets: [hydrogenPreset()],
+  presets: [patchedHydrogenPreset()],
   appDirectory: "app",
   buildDirectory: "dist",
   ssr: true,
