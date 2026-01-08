e7bbebf660d7bb5750bddd3c89ebb6edcfd50e3f
Brooks Lybrand <brookslybrand@gmail.com>
2026-01-08 16:05:57 -0600

    Try updating the package again

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,32 +1,8 @@
 import type { Config } from "@react-router/dev/config";
 import { hydrogenPreset } from "@shopify/hydrogen/react-router-preset";
 
-// Renames stabilized future flags from Hydrogen's outdated unstable_ names
-function patchedHydrogenPreset() {
-  const preset = hydrogenPreset();
-  return {
-    ...preset,
-    async reactRouterConfig(
-      args: Parameters<NonNullable<typeof preset.reactRouterConfig>>[0],
-    ) {
-      const config = await preset.reactRouterConfig!(args);
-
-      return {
-        ...config,
-        future: {
-          v8_middleware: true,
-          v8_splitRouteModules: true,
-          v8_viteEnvironmentApi: false,
-          unstable_optimizeDeps: true,
-          unstable_subResourceIntegrity: false,
-        } satisfies Config["future"],
-      };
-    },
-  };
-}
-
 export default {
-  presets: [patchedHydrogenPreset()],
+  presets: [hydrogenPreset()],
   appDirectory: "app",
   buildDirectory: "dist",
   ssr: true,
