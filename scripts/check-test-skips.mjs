ea111b3cf9d0661627ddf0cc260cee375ffcf87a
Brooks Lybrand <brookslybrand@gmail.com>
2026-08-13 09:46:48 -0500

    test commit

--- scripts/check-test-skips.mjs
+++ scripts/check-test-skips.mjs
@@ -1,6 +1,8 @@
 import { readFile } from "node:fs/promises";
 import { glob } from "node:fs/promises";
 
+console.log("just testing");
+
 let skipped = [];
 for await (let file of glob(["app/**/*.{ts,tsx}", "e2e/**/*.ts"])) {
   let source = await readFile(file, "utf8");
